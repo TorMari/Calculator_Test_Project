55de91da35c4d3c8ae5b2515c4ba46c5ce93cd2c
TorMari <mariia.torska.kn.2021@lpnu.ua>
2023-09-14 15:09:48 +0300

    Добавьте файлы проекта.

---
+++ AnalizerClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalizerClassLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalizerClassLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6df8818b-1765-4bd4-9a05-b8108cb239d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AnalizerClassLibraryTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnalaizerClassLibrary;
+using System;
+using System.Data;
+
+namespace AnalizerClassLibraryTests
+{
+    [TestClass]
+    public class AnalizerClassTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DataSource("System.Data.SqlClient",
+            "Data Source=WIN-8SNIO6IEEFI;Initial Catalog=CalculatorTests;Integrated Security=True",
+            "tests",
+            DataAccessMethod.Sequential)]
+        public void AnalaizerClass_Estimate_FormDB()
+        {
+            string expression = TestContext.DataRow["expression"].ToString();
+            string expected = TestContext.DataRow["result"].ToString();
+            string res = "";
+            try
+            {
+                AnalaizerClass.expression = expression;
+                res = AnalaizerClass.Estimate();
+                Assert.AreEqual(expected, res);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(res.StartsWith("&Error"));
+            }
+        }
+    }
+}
+
